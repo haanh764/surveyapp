63ef0c56d1d6a7de863579de9eb41ed179d85fa3
haanh764 <ngohaanh764@gmail.com>
2022-05-21 17:46:19 +0200

    update docker nginx

--- ui/src/views/AboutView.vue
+++ ui/src/views/AboutView.vue
@@ -12,7 +12,7 @@ export default {
     message: String
   }),
   created() {
-    fetch("http://localhost:5000")
+    fetch("http://localhost:8000")
       .then (response => response.json())
       .then (data => {
         this.message = data.message
