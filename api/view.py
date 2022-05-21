d365080a3b47ff56b8f65f092c1bba76dd57774d
haanh764 <ngohaanh764@gmail.com>
2022-05-21 17:21:48 +0200

    adding routes

--- api/view.py
+++ api/view.py
@@ -2,4 +2,4 @@ from flask_restful import Resource
 
 class Home(Resource):
   def get(self):
-    return {'message': 'Hello, World!'}
+    return {'message': 'Hello, World! Welcome to the API for SurveyAPP.'}
---
+++ ui/src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutView',
+  data: () => ({
+    message: String
+  }),
+  created() {
+    fetch("http://localhost:5000")
+      .then (response => response.json())
+      .then (data => {
+        this.message = data.message
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+};
+</script>
