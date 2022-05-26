e13f052e511407ba2b2caa14525660f9cfd0674e
artikandri <argikartika@gmail.com>
2022-05-26 22:04:22 +0200

    feat: added service worker

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,10 +3,12 @@ import axios from "axios";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import "./plugins";
 import store from "./store";
 import axiosConfig from "./api/config.js";
 import { sync } from "vuex-router-sync";
+
+import "./plugins";
+import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios.create(axiosConfig);
