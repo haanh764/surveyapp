1619c2f8d4dbbbe4993e2b0444bfcde02cf26180
Teddy Ferdinan <64476430+teddy-f-47@users.noreply.github.com>
2022-05-28 11:46:01 +0200

    Finished the 404 page
    What's changed:
    - ui/src/assets/svg/404.png : Replaced the png file with a new one with a higher resolution.
    - ui/src/views/PageNotFoundView.vue : Added the 404 image and text, finished styling the page.
    
    To do:
    - ui/src/views/PageNotFoundView.vue : Redirect after 3 seconds based on user type. However, in my opinion, the "back to home" button alone is sufficient, so the redirection will only be added later if we have enough time.

--- ui/src/views/PageNotFoundView.vue
+++ ui/src/views/PageNotFoundView.vue
@@ -6,12 +6,19 @@
   >
     <v-row justify="center">
       <v-col cols="auto">
-        page not found
+        <h1>Page not found</h1>
+        <p>The page either doesn't exist or you don't have access to this page</p>
+        <center>
+          <v-img :src="mainPicture" :width="imageWidth" contain />
+        </center>
         <!-- to do: check the log in state, redirect based on the user type -->
         <v-btn
           depressed
           large
           to="/"
+          :color="variables.default.primary"
+          :width="imageWidth"
+          class="white--text"
         >
           back to home
         </v-btn>
@@ -21,11 +28,21 @@
 </template>
 
 <script>
-  export default { name: "ErrorView" };
+  export default { name: "ErrorView",
+  data: function() {
+    return {
+      imageWidth: "35%"
+    }
+  },
+  computed: {
+    variables() {
+      return require("@styles/variables.js");
+    },
+    mainPicture() {
+      return require("@assets/svg/404.png");
+    }
+  } };
 </script>
 
 <style lang="scss">
-  #error-view h1.text-h1 {
-    font-size: 14rem !important
-  }
 </style>
