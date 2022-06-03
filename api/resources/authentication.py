008df785fd80097d250823f6428d77234819da6f
haanh764 <ngohaanh764@gmail.com>
2022-06-03 17:56:46 +0200

    feat: auth signup - email validation

--- api/resources/authentication.py
+++ api/resources/authentication.py
@@ -4,18 +4,23 @@ from flask_jwt_extended import create_access_token
 
 from models.user import User
 import datetime
+from email_validator import validate_email, EmailNotValidError
 
 class SignUp(Resource):
     def post(self):
         data = request.get_json()
-        existing_user = User.find_by_email(data['email'])
-        if existing_user:
-            return {'message': 'User {} already exists. Please Login or Signup with another email!'.format(data['email'])}
-        user = User(**data)
-        user.generate_password()
-        user.add_user()
-        id = user.id
-        return {'message': 'User {} was created'.format(id)}, 200
+        try:
+            validate_email(data['email'])
+            existing_user = User.find_by_email(data['email'])
+            if existing_user:
+                return {'message': 'User {} already exists. Please Login or Signup with another email!'.format(data['email'])}
+            user = User(**data)
+            user.generate_password()
+            user.add_user()
+            id = user.id
+            return {'message': 'User {} was created'.format(id)}, 200
+        except EmailNotValidError as errorMsg:
+            return {'message': 'Invalid email address. {}'.format(errorMsg)}, 400
 
 class Login(Resource):
     def post(self):
