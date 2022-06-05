1ffefcc9925d8830411490b7312516c32c8b83f7
haanh764 <ngohaanh764@gmail.com>
2022-06-05 17:07:02 +0200

    feat: api user - change password

--- api/resources/routes.py
+++ api/resources/routes.py
@@ -1,5 +1,6 @@
 from resources.authentication import Login, SignUp, Logout, ActivateAccount, NotActivated, ResendActivation
 from resources.view import Home
+from resources.user import ChangePassword
 
 def initialize_routes(api):
     api.add_resource(Home, '/api/home')
@@ -10,3 +11,5 @@ def initialize_routes(api):
     api.add_resource(ActivateAccount, '/api/authentication/activate/<string:token>')
     api.add_resource(NotActivated, '/api/authentication/notactivated')
     api.add_resource(ResendActivation, '/api/authentication/resend')
+
+    api.add_resource(ChangePassword, '/api/user/changepassword')
