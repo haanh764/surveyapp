3193d7df8eef5d8a2523f1e1a84c2372aaa57a9e
haanh764 <ngohaanh764@gmail.com>
2022-06-06 19:58:59 +0200

    feat: api admin - add amin model

--- api/common/authentication_helper.py
+++ api/common/authentication_helper.py
@@ -1,6 +1,9 @@
+from functools import wraps
 from itsdangerous import URLSafeTimedSerializer
 from flask_mail import Message
-from app import app, mail
+from app import app, mail, jwt
+from common.settings import MAIL_USERNAME
+from flask_jwt_extended import get_jwt, verify_jwt_in_request
 
 def generate_confirmation_token(email):
     serializer = URLSafeTimedSerializer(app.config['JWT_SECRET_KEY'])
@@ -22,3 +25,24 @@ def send_email(to, subject, confirm_url):
     msg = Message(subject, sender=app.config['MAIL_USERNAME'], recipients=[to])
     msg.body = 'Your confirmation link is here: {}'.format(confirm_url)
     mail.send(msg)
+
+# def validate_admin_email(email):
+#     def decorator(fn):
+#         @wraps(fn)
+#         def wrapper(*args, **kwargs):
+#             if email == MAIL_USERNAME:
+#                 return fn(*args, **kwargs)
+#             else:
+#                 return {'message': 'You are not authorized to perform this action!'}, 403
+
+def admin_required():
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            verify_jwt_in_request()
+            if get_jwt()['email'] == MAIL_USERNAME:
+                return fn(*args, **kwargs)
+            else:
+                return {'message': 'You are not authorized to perform this action!'}, 403
+        return wrapper
+    return decorator
