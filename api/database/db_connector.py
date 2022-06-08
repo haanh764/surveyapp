6a971b8e53c60559b4840fe6b3a1a6cbaae2953c
Daedheldir <marcin.bernacki316@gmail.com>
2022-06-08 18:48:45 +0200

    no message

--- api/database/db_connector.py
+++ api/database/db_connector.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine
+from typing import Literal
+from sqlalchemy import create_engine, select, insert, MetaData
+from sqlalchemy.engine.cursor import CursorResult
 from common.settings import DB_URL
 
 from datetime import date, datetime
@@ -8,12 +10,32 @@ class DbConnector():
     def __init__(self):
         self.engine = create_engine(DB_URL)
         self.connection = self.engine.connect()
-    
+        self.meta_data = MetaData(bind=self.connection, reflect=True)
+
     def get_connection(self):
         return self.connection
-    
-    def close_connection(self):
+        
+    def __open_connection__(self):
+        self.connection = self.engine.connect()
+
+    def __run_query__(self, query) -> CursorResult:
+        self.__open_connection__()
+        result = self.connection.execute(query)
+        self.__close_connection__()
+        return result
+
+    def __close_connection__(self):
         self.connection.close()
+
+    def add_user(self, user_email: Literal, user_password: Literal):
+        users_table = self.meta_data.tables['users']
+        query = insert(users_table).values(email=user_email, password=user_password)
+        self.__run_query__(query)
+
+    def get_user(self, user_email: Literal) -> CursorResult:
+        users_table = self.meta_data.tables['users']
+        query = select(users_table).where(users_table)
+        return self.__run_query__(query)
     
     def clean_select_row(self, row, keys):
         try:
